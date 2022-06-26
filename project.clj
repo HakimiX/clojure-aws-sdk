76cfa279dc3950543664e4d6505479e950a65639
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-26 13:21:44 +0200

    add aws secrets-manager library

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/HakimiX/clojure-aws-sdk"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.amazonaws/aws-java-sdk-s3 "1.12.239"]
-                 [com.amazonaws/aws-java-sdk-lambda "1.12.239"]]
+                 [com.amazonaws/aws-java-sdk-lambda "1.12.239"]
+                 [com.amazonaws/aws-java-sdk-secretsmanager "1.12.239"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
