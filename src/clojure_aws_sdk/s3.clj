f6243883a006a8fc000da844e7c449a2284f20e5
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-26 13:03:04 +0200

    add doc-strings

--- src/clojure_aws_sdk/s3.clj
+++ src/clojure_aws_sdk/s3.clj
@@ -7,12 +7,15 @@
 
 (defn object-exist?
   "Takes the name of the bucket containing the object and the name
-  of the object that has to be checked."
+  of the object that has to be checked.
+  Returns the value true if the specified object exists in the Amazon S3 bucket; the value
+  false if the object doesn't exist in the specified Amazon S3 bucket"
   [^AmazonS3Client client bucket-name object-name]
   (.doesObjectExist client bucket-name object-name))
 
 (defn bucket-exist?
-  "Takes the name of the bucket to check"
+  "Takes the name of the bucket to check.
+  Returns the value true if the specified bucket exists in Amazon S3; the value false
+  if there is no bucket in Amazon S3 with that name."
   [^AmazonS3Client client bucket-name]
   (.doesBucketExistV2 client bucket-name))
-
