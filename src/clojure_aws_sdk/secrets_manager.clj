c1721caa7e77324970a90fbb76bb23bcfe9ef53d
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-26 13:38:29 +0200

    add aws secrets manager todo list

--- src/clojure_aws_sdk/secrets_manager.clj
+++ src/clojure_aws_sdk/secrets_manager.clj
@@ -1,1 +1,14 @@
-(ns clojure-aws-sdk.secrets-manager)
+(ns clojure-aws-sdk.secrets-manager
+  (:import (com.amazonaws.services.secretsmanager AWSSecretsManagerClientBuilder)))
+
+;; TODO: implement support for "standard" client
+(def client (AWSSecretsManagerClientBuilder/defaultClient))
+
+;; TODO: implement the following Amazon Secrets Manager functionality
+;; create-secret
+;; delete-secret
+;; describe-secret
+;; get-secret-value
+;; list-secrets
+;; update-secret
+
