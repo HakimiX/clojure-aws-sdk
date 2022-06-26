cd83e36be373d34490a475208893118dbcb89628
HakimiX <mustafa.hakimi@outlook.dk>
2022-06-26 12:48:40 +0200

    remove unnecessary keys from dependency file and add url and description

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject clojure-aws-sdk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject clojure-aws-sdk "0.1.0"
+  :description "Clojure AWS SDK for Java"
+  :url "https://github.com/HakimiX/clojure-aws-sdk"
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.amazonaws/aws-java-sdk-s3 "1.12.239"]
                  [com.amazonaws/aws-java-sdk-lambda "1.12.239"]]
-  :main ^:skip-aot clojure-aws-sdk.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
