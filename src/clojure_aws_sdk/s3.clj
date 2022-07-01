a861936398e1e4a0467cfb61de9d99b68af9b201
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-01 18:35:46 +0200

    refactor doc-strings

--- src/clojure_aws_sdk/s3.clj
+++ src/clojure_aws_sdk/s3.clj
@@ -6,16 +6,14 @@
 (def client (AmazonS3ClientBuilder/defaultClient))
 
 (defn object-exist?
-  "Takes the name of the bucket containing the object and the name
-  of the object that has to be checked.
-  Returns the value true if the specified object exists in the Amazon S3 bucket;
-  the value false if the object doesn't exist in the specified Amazon S3 bucket"
+  "Takes the name of the bucket containing the object and the name of the object
+  that has to be checked. Returns the value true if the specified object exists in
+  bucket; the value false if the object doesn't exist in the specified bucket."
   [^AmazonS3Client client ^String bucket-name ^String object-name]
   (.doesObjectExist client bucket-name object-name))
 
 (defn bucket-exist?
-  "Takes the name of the bucket to check.
-  Returns the value true if the specified bucket exists in Amazon S3; the value false
-  if there is no bucket in Amazon S3 with that name."
+  "Takes the name of the bucket to check. Returns the value true if the
+  specified bucket exists; the value false if there is no bucketwith that name."
   [^AmazonS3Client client ^String bucket-name]
   (.doesBucketExistV2 client bucket-name))
