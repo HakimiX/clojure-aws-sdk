09f6277682179dba2aae986982fc9e03d33e411f
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-03 13:48:38 +0200

    add s3 todos

--- src/clojure_aws_sdk/s3.clj
+++ src/clojure_aws_sdk/s3.clj
@@ -2,6 +2,13 @@
   (:import (com.amazonaws.services.s3 AmazonS3ClientBuilder AmazonS3Client)))
 
 ;; TODO use standrd AmazonS3ClientBuilder with credentials-provider
+;; TODO: implement S3 operations
+;; Upload an Object
+;; List Objects
+;; Download an Object
+;; Copy, Move, or Rename Objects
+;; Delete an Object
+;; Delete Multiple Objects at Once
 
 (def client (AmazonS3ClientBuilder/defaultClient))
 
