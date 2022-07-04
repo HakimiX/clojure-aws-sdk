c957cd828d47be5f765a5680bea875c6104a0a8d
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-04 19:16:08 +0200

    add create  bucket function

--- src/clojure_aws_sdk/s3.clj
+++ src/clojure_aws_sdk/s3.clj
@@ -21,18 +21,6 @@
       (BasicSessionCredentials. (:access-key creds) (:secret-key creds) (:token creds))
       (BasicAWSCredentials. (:access-key creds) (:secret-key creds)))))
 
-(defn object-exist?
-  [^AmazonS3Client client ^String bucket ^String object]
-  (.doesObjectExist client bucket object))
-
-(defn bucket-exist?
-  [^AmazonS3Client client ^String bucket]
-  (.doesBucketExistV2 client bucket))
-
-(defn delete-bucket
-  [cred ^String bucket]
-  (.deleteBucket (client cred) bucket))
-
 (defprotocol ^{:no-doc true} Mappable
   "Convert a value into a Clojure map."
   (^{:no-doc true} to-map [x] "Return a map of the value."))
@@ -49,6 +37,22 @@
      :display-name (.getDisplayName owner)})
   nil
   (to-map [_] nil))
+
+(defn object-exist?
+  [^AmazonS3Client client ^String bucket ^String object]
+  (.doesObjectExist client bucket object))
+
+(defn bucket-exist?
+  [^AmazonS3Client client ^String bucket]
+  (.doesBucketExistV2 client bucket))
+
+(defn create-bucket
+  [cred ^String name]
+  (to-map (.craeteBucket (client cred) name)))
+
+(defn delete-bucket
+  [cred ^String bucket]
+  (.deleteBucket (client cred) bucket))
 
 (defn list-buckets
   "List all the S3 buckets for the supplied credentials. The buckets will be
