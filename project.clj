5fb3e8439b352310127620a24737c526200eed88
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-08 22:02:07 +0200

    add function to get ssm parameter from aws

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
                  [cheshire "5.11.0"]
                  [com.amazonaws/aws-java-sdk-s3 "1.12.239"]
                  [com.amazonaws/aws-java-sdk-lambda "1.12.239"]
-                 [com.amazonaws/aws-java-sdk-secretsmanager "1.12.239"]]
+                 [com.amazonaws/aws-java-sdk-secretsmanager "1.12.239"]
+                 [com.amazonaws/aws-java-sdk-ssm "1.12.239"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
---
+++ src/clojure_aws_sdk/ssm.clj
@@ -0,0 +1,12 @@
+(ns clojure-aws-sdk.ssm
+  (:import (com.amazonaws.services.simplesystemsmanagement.model GetParameterRequest)
+           (com.amazonaws.services.simplesystemsmanagement AWSSimpleSystemsManagementClient AWSSimpleSystemsManagement)))
+
+(defn get-parameter
+  [^AWSSimpleSystemsManagement client paramter]
+  (let [ssm-request (-> (GetParameterRequest.)
+                        (.withName paramter)
+                        (.withWithDecryption true))]
+    (-> (.getParameter client ssm-request)
+        (.getParameter)
+        (.getValue))))
