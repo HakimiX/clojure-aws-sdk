3cbc0307268dbed8457c14b657cfef9760cda17f
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-09 15:33:06 +0200

    add lambda client and sqs namespace

--- src/clojure_aws_sdk/aws/lambda.clj
+++ src/clojure_aws_sdk/aws/lambda.clj
@@ -1,10 +1,23 @@
 (ns clojure-aws-sdk.aws.lambda
-  (:import (com.amazonaws.services.lambda AWSLambdaClient)))
+  (:import (com.amazonaws.services.lambda AWSLambdaClient)
+           (com.amazonaws ClientConfiguration)
+           (com.amazonaws.auth BasicAWSCredentials DefaultAWSCredentialsProviderChain)))
+
+(defn lambda-client*
+  "Creates an AWSLambdaClient instance from a map of credentials and client configuration
+  parameters such as connection timeout and proxy settings."
+  [{:keys [access-key secret-key endpoint]}]
+  (let [creds (if (and access-key secret-key)
+                (BasicAWSCredentials. access-key secret-key)
+                (DefaultAWSCredentialsProviderChain.))
+        client (AWSLambdaClient. creds)]
+    (if endpoint (.setEndpoint client endpoint))
+    client))
 
 (defn list-functions
   "Returns a list of lambda functions, with the version-specific configuration
   for each function."
   [^AWSLambdaClient client]
   (->> (.listFunctions client)
-       (.getFunctions)
-       (map #(.getFunctionName %))))
+       #_(.getFunctions)
+       #_(map #(.getFunctionName %))))
---
+++ src/clojure_aws_sdk/aws/sqs.clj
@@ -0,0 +1,4 @@
+(ns clojure-aws-sdk.aws.sqs)
+
+;; Examples
+;; https://github.com/pingles/clj-aws/blob/master/src/clj_aws/sqs.clj
