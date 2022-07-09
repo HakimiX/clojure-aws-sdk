912a12454ff31f4918a617a027a8e8a8974387e9
HakimiX <mustafa.hakimi@outlook.dk>
2022-07-09 12:58:57 +0200

    add lambda terraform iac

--- iac/main.tf
+++ iac/main.tf
@@ -18,3 +18,42 @@ resource "aws_s3_bucket_object" "sample_bucket_object" {
   depends_on = [aws_s3_bucket.sample_bucket]
 }
 
+# --------------------------
+# AWS Lambda
+# --------------------------
+
+resource "aws_lambda_function" "sample_lambda_function" {
+  function_name = "bamboozle-sample-lambda"
+  role          = aws_iam_role.sample_lambda_role.arn
+  runtime       = "java8"
+  memory_size   = "1536"
+
+  // Code
+  handler       = "bamboozle-sample-lambda.lambda.handler"
+  filename      = "bamboozle-sample-lambda.jar"
+  source_code_hash = filebase64sha256("bamboozle-sample-lambda.jar")
+
+  environment {
+    variables = {
+      API_ENDPOINT = "https://jsonplaceholder.typicode.com/"
+    }
+  }
+}
+
+// Lambda role
+resource "aws_iam_role" "sample_lambda_role" {
+  name               = "bamboozle-sample-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.AWSLambdaTrustPolicy.json
+}
+
+// Lambda policy
+data "aws_iam_policy_document" "AWSLambdaTrustPolicy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+}
